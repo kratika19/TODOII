d5f2dd9d0dbff4cfe52bd9b9241127202072e8b5
KRATIKA MITTAL <56781757+kratika19@users.noreply.github.com>
2021-05-26 14:54:54 +0530

    minimal footer added with styles

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 export default function Footer() {
+    let footerStyle = {
+        position: "absolute",
+        top: "100vh",
+        width: "100%"
+    }
+
     return (
-        <div className="bg-dark text-light">
+        <footer className="bg-dark text-light py-3" style={footerStyle}>
             <p className="text-center">
                 Copyright &copy; Todoii.com
             </p>
-        </div>
+        </footer>
     )
 }
